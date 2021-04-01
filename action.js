692b2da2744795c2995c354ea0b2e022da3b2005
DAC <dac@cogsmith.com>
2021-04-01 18:22:32 -0400

    .

---
+++ action.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+
+console.log('ACTION');
+
+console.log();
+
+Object.keys(process.env).forEach(x=>{
+  console.log(x);
+  console.log(process.env[x]);
+  console.log();
+});
+
+const AppPackage = require('./package.json');
+console.log(AppPackage);
+
+if (!AppPackage.version) { AppPackage.version='0.0.0'; }
+
+// fs.writeFileSync('./package.json',JSON.stringify(AppPackage));
+
+console.log();
+
+const { Octokit } = require("@octokit/rest");
+const octokit = new Octokit();
+
+// Compare: https://docs.github.com/en/rest/reference/repos/#list-organization-repositories
+octokit.rest.repos.listForOrg({org: "octokit",type: "public",}).then(({ data }) => {
+    //console.log(data);
+});
