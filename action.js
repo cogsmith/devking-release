80436b5047f6abe81787f61bae168fa479f5a3ff
DAC <dac@cogsmith.com>
2021-04-01 19:49:14 -0400

    .

--- action.js
+++ action.js
@@ -39,5 +39,5 @@ const octokit = new Octokit();
 
 // Compare: https://docs.github.com/en/rest/reference/repos/#list-organization-repositories
 octokit.rest.repos.listForOrg({ org: "octokit", type: "public", }).then(({ data }) => {
-    console.log(data);
+    //console.log(data);
 });
