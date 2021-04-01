bb8a9dc148ac5d0e53b698ba6de941031c7bbc4a
DAC <dac@cogsmith.com>
2021-04-01 19:09:12 -0400

    .

--- action.js
+++ action.js
@@ -4,21 +4,31 @@ console.log('ACTION');
 
 console.log();
 
-Object.keys(process.env).forEach(x=>{
-  // console.log(x); console.log(process.env[x]); console.log();
+Object.keys(process.env).forEach(x => {
+    // console.log(x); console.log(process.env[x]); console.log();
 });
 
 const AppPackage = require('./package.json');
-//console.log(AppPackage);
+console.log(AppPackage);
 
-if (!AppPackage.version) { AppPackage.version='0.0.0'; }
+if (!AppPackage.version) { AppPackage.version = '0.0.0'; }
 
-fs.writeFileSync('./package.json',JSON.stringify(AppPackage));
+let vz = [AppPackage.version.split('-')[0]].concat(AppPackage.version.split('.'));
+
+let bumplevel = 'PATCH';
+
+if (bumplevel == 'PATCH') { vz[3]++; }
+
+AppPackage.version = vz.slice(1).join('.'); if (vz[0]) { AppPackage.version += '-' + vz[0]; };
+
+console.log(AppPackage);
+
+fs.writeFileSync('./package.json', JSON.stringify(AppPackage));
 
 const { Octokit } = require("@octokit/rest");
 const octokit = new Octokit();
 
 // Compare: https://docs.github.com/en/rest/reference/repos/#list-organization-repositories
-octokit.rest.repos.listForOrg({org: "octokit",type: "public",}).then(({ data }) => {
+octokit.rest.repos.listForOrg({ org: "octokit", type: "public", }).then(({ data }) => {
     //console.log(data);
 });
