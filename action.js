abdaa74e2df23c34ad5ec2dad18599bff07415cd
DAC <dac@cogsmith.com>
2021-04-03 08:09:59 -0400

    .

--- action.js
+++ action.js
@@ -201,12 +201,12 @@ App.GetLogMD = function (itemdb) {
 App.FXFX = async function () { await App.FX(); App.CMD(); }
 
 App.CMD = function () {
-    execa.commandSync('date >> dt.txt',{shell:true});
-    execa.commandSync(' npm version patch --no-git-tag-version ; npm version patch --no-git-tag-version',{shell:true});
+    execa.commandSync('date >> dt.txt', { shell: true });
+    execa.commandSync(' npm version patch --no-git-tag-version ; npm version patch --no-git-tag-version', { shell: true });
     execa.commandSync('git config user.name DEVKING');
     execa.commandSync('git config user.email devkingbot@cogsmith.com');
     // execa.commandSync('echo ' + GITHUB_TOKEN + ' | gh auth login --with-token',{shell:true});
-    execa.commandSync('gh release delete 9.9.9 --yes');
+    try { execa.commandSync('gh release delete 9.9.9 --yes'); } catch (ex) {}
     execa.commandSync('gh release create 9.9.9 --target main');
     execa.commandSync('git add .');
     execa.commandSync("git commit -m 'DT'");
