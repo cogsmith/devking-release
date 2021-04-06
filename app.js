0be5c2ecb9c43630e896ee51035ecce53be1f287
DAC <dac@cogsmith.com>
2021-04-06 15:44:51 -0400

    .

--- app.js
+++ app.js
@@ -82,10 +82,10 @@ App.Init = async function () {
     // Object.keys(process.env).sort().forEach(x => { if (x.startsWith('GITHUB')) { LOG.TRACE(x + ': ' + process.env[x]); } });
 
     let repoinfofile = process.cwd() + '/package.json';
-    if (!fs.existsSync(repoinfofile)) { LOG.WARN('MISSING PACKAGE.JSON'); fs.writeFileSync(repoinfofile, '{"version":"0.0.0"}' + "\n"); }
+    if (!fs.existsSync(repoinfofile)) { LOG.WARN('MISSING PACKAGE.JSON'); fs.writeFileSync(repoinfofile, '{"version":"0.0.1"}' + "\n"); }
 
     let repoinfo = require(repoinfofile);
-    if (!repoinfo.version) { LOG.WARN('MISSING REPOINFO.VERSION'); repoinfo.version = '0.0.0'; }
+    if (!repoinfo.version) { LOG.WARN('MISSING PACKAGE.VERSION'); repoinfo.version = '0.0.1'; }
 
     //let nextv = core.getInput('NEXTVERSION').toLowerCase() || 'patch';
     let nextv = process.env.NEXTVERSION || 'patch'; nextv = nextv.toLowerCase();
