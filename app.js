eb9b00c07fa2060b2e411277226e6d296e4eda03
DAC <dac@cogsmith.com>
2021-04-06 12:37:09 -0400

    .

--- app.js
+++ app.js
@@ -73,7 +73,7 @@ App.Init = async function () {
 
     if (nextv == 'patch' || nextv == 'minor' || nextv == 'major') {
         VNOW = repoinfo.version;
-        VTAG = VNOW.includes('-') ? semver.inc(VNOW, 'patch') : VNOW;
+        VTAG = VNOW.includes('-') ? VNOW.split('-')[0] : VNOW;
         VNEXT = semver.inc(VTAG, nextv) + '-dev';
         VLAST = repoinfo.versiontaglast;
     }
