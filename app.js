ff24d283436d2930032005584237dbea928d7cf3
DAC <dac@cogsmith.com>
2021-04-06 12:01:48 -0400

    .

--- app.js
+++ app.js
@@ -236,8 +236,10 @@ App.FX = async function () {
 App.GetLogTXT = function (itemdb) {
     let txt = [];
     txt.push('# ' + VTAG + ' @ ' + VDATE); txt.push(null);
-    txt.push('## DIFF');
-    txt.push('- ' + (VDIFF >= 0 ? VDIFF + ' COMMITS SINCE' : '') + ' LAST TAG' + (VLAST ? ' = ' + VLAST : ''));
+    if (VDIFF >= 0 || VLAST) {
+        txt.push('## DIFF');
+        txt.push('- ' + (VDIFF >= 0 ? VDIFF + ' COMMITS SINCE' : '') + ' LAST TAG' + (VLAST ? ' = ' + VLAST : ''));
+    }
     Object.keys(itemdb).forEach(k => {
         txt.push('## ' + k); // txt.push(null);
         itemdb[k].forEach(z => {
@@ -256,8 +258,10 @@ App.GetLogMD = function (itemdb) {
     let txt = [];
     txt.push('<code>'); txt.push(null);
     txt.push('# [' + VTAG + ' @ ' + VDATE + '](https://github.com/' + GITHUB_REPOTEAM + '/' + GITHUB_REPONAME + '/releases/tag/' + VTAG + ')');
-    txt.push('## DIFF');
-    txt.push('- [' + (VDIFF >= 0 ? VDIFF + ' COMMITS SINCE' : '') + ' LAST TAG' + (VLAST ? ' = ' + VLAST : '') + '](https://github.com/cogsmith/test-actions/compare/' + (VLAST ? VLAST : '0.0.0') + '...' + VTAG + ')');
+    if (VDIFF >= 0 || VLAST) {
+        txt.push('## DIFF');
+        txt.push('- [' + (VDIFF >= 0 ? VDIFF + ' COMMITS SINCE' : '') + ' LAST TAG' + (VLAST ? ' = ' + VLAST : '') + '](https://github.com/cogsmith/test-actions/compare/' + (VLAST ? VLAST : '0.0.0') + '...' + VTAG + ')');
+    }
     let keyi = 0; Object.keys(itemdb).forEach(k => {
         if (keyi++ > -1) { txt.push(null); txt.push('---'); txt.push(null); }
         txt.push('## ' + k); // txt.push(null);
