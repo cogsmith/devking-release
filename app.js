d4f36dddb00e31314e7e6aa463f0388da7ce528e
DAC <dac@cogsmith.com>
2021-05-12 18:20:32 -0400

    DEV

--- app.js
+++ app.js
@@ -445,7 +445,7 @@ App.CMD = async function () {
 
     if (fs.existsSync('.vscodeignore')) {
         cmdz = [];
-        cmdz.push('node .\node_modules\vsce\out\vsce publish -p ' + process.env['NPM_TOKEN']);
+        cmdz.push('node /home/runner/work/_actions/cogsmith/devking-release/main/node_modules/vsce/out/vsce publish -p ' + process.env['NPM_TOKEN']);
         App.RunCMDS(cmdz);
     }
 
